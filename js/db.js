30488397dbed0410da8ddfdd42874ee77aa918e1
anastasia-p <anastasia.aap@gmail.com>
2026-03-28 15:34:35 +0300

    фикс дб

--- js/db.js
+++ js/db.js
@@ -1,7 +1,17 @@
 // Firebase init and data access layer
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyB0898njk5wWWEZgTYdQcg0Rv_loFIcH2c',
+  authDomain: 'spring-tracker.firebaseapp.com',
+  projectId: 'spring-tracker',
+  storageBucket: 'spring-tracker.firebasestorage.app',
+  messagingSenderId: '192640819831',
+  appId: '1:192640819831:web:34ecfca0a22434f0271c72'
+});
+
 var db = firebase.firestore();
 db.enablePersistence().catch(function() {});
+
 
 // In-memory cache: section -> dateKey -> {plan, type, label, checks, values}
 var cache = { strength: {}, wingchun: {}, qigong: {} };
