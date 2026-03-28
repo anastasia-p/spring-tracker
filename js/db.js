bb8259c82f20400dd5fe209873c93912cce1f3be
anastasia-p <anastasia.aap@gmail.com>
2026-03-28 18:53:19 +0300

    фикс большого бага с регистрацией

--- js/db.js
+++ js/db.js
@@ -1,14 +1,5 @@
 // Firebase init and data access layer
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyB0898njk5wWWEZgTYdQcg0Rv_loFIcH2c',
-  authDomain: 'spring-tracker.firebaseapp.com',
-  projectId: 'spring-tracker',
-  storageBucket: 'spring-tracker.firebasestorage.app',
-  messagingSenderId: '192640819831',
-  appId: '1:192640819831:web:34ecfca0a22434f0271c72'
-});
-
 var db = firebase.firestore();
 db.enablePersistence().catch(function() {});
 
---
+++ js/firebase-init.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+  apiKey: 'AIzaSyB0898njk5wWWEZgTYdQcg0Rv_loFIcH2c',
+  authDomain: 'spring-tracker.firebaseapp.com',
+  projectId: 'spring-tracker',
+  storageBucket: 'spring-tracker.firebasestorage.app',
+  messagingSenderId: '192640819831',
+  appId: '1:192640819831:web:34ecfca0a22434f0271c72'
+});
